4cba4c26166c664c8563046be422bda6ca725521
fate-day <1485327282@qq.com>
2020-05-28 18:25:30 +0800

    更新3

--- src/main/java/com/example/team/dao/TeamDAO.java
+++ src/main/java/com/example/team/dao/TeamDAO.java
@@ -1,9 +1,23 @@
 package com.example.team.dao;
 
 import com.example.team.pojo.Team;
+import com.example.team.pojo.User;
+
+import java.util.List;
+import java.util.Set;
 
 public interface TeamDAO {
-    void createTeam(Team team);
-    void updateTeam(Team team);
-    void deleteTeam(int teamId);
+    void add(Team team);
+
+    boolean update(Team team);
+
+    boolean delete(int teamId);
+
+    Set<User> getMembers();
+
+    boolean addMember(int teamId, int userId);
+
+    boolean inviteMember(int teamId, int userId);
+    
+    boolean deleteMember(int teamId, int userId);
 }
